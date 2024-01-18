da3416cf942d9c1ced337a4b361f898a6a1d3ead
Aliyan Gohar <a.gohar@mail.utoronto.ca>
2024-01-18 12:24:04 -0500

    Fix change

--- base/models.py
+++ base/models.py
@@ -2,15 +2,7 @@ from django.db import models
 import datetime
 from django.utils import timezone
 from django.core.validators import MaxValueValidator, MinValueValidator
-class food(models.Model):
-    day = models.ForeignKey("macro_day", on_delete=models.CASCADE, default=1)
-    name = models.CharField("Name", default="", max_length=302)
-    cal = models.IntegerField("Calories", default=0)
-    protein = models.IntegerField(default=0)
-    carbs = models.IntegerField(default = 0)
-    fat = models.IntegerField(default=0)
-    def __str__(self):
-        return str(self.cal) + "calories"
+
 
 class user_goals(models.Model):
     owner = models.OneToOneField("accounts.User", on_delete=models.CASCADE, default=1)
@@ -46,13 +38,21 @@ class macro_day(models.Model):
     def __str__(self):
         return str(self.date) + ": " + str(self.calories) + str(self.weight)
     class Meta:
-        constraints = [models.UniqueConstraint(fields=['owner', 'date'], name="Unique tuple")
-                       , models.UniqueConstraint(fields=['date', 'owner'], name="Unique owner date tuple")]
+        constraints = [models.UniqueConstraint(fields=['owner', 'date'], name="Unique_tuple")]
     
+class food(models.Model):
+    day = models.ForeignKey("macro_day", on_delete=models.CASCADE, default=1)
+    name = models.CharField("Name", default="", max_length=302)
+    cal = models.IntegerField("Calories", default=0)
+    protein = models.IntegerField(default=0)
+    carbs = models.IntegerField(default = 0)
+    fat = models.IntegerField(default=0)
+    def __str__(self):
+        return str(self.cal) + "calories"
 
 class savedFood(models.Model):
     owner = models.ForeignKey('accounts.User', on_delete=models.CASCADE, default=1)
-    food_name = models.TextField("Name", default="")
+    food_name = models.CharField(default="", max_length=300)
     cal_100g = models.IntegerField("Calories Per 100g", default=0)
     protein_100g = models.IntegerField("Protein per 100g", default=0)
     carb_100g = models.IntegerField("Carbohydrates per 100g", default=0)
@@ -60,7 +60,7 @@ class savedFood(models.Model):
     def __str__(self):
         return self.food_name
     class Meta:
-        constraints = [models.UniqueConstraint(fields=['owner', 'food_name'], name="Unique food tuple")
-                       ,models.UniqueConstraint(fields=['food_name', 'owner'], name="Unique owner food tuple")]
-    
+        constraints = [
+            models.UniqueConstraint(fields=['owner', 'food_name'], name='unique_food_tuple')
+        ]
 
