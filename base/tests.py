aed67ac39ee3823f294e10dce7bc1b36d6956923
Aliyan Gohar <a.gohar@mail.utoronto.ca>
2024-01-21 15:31:38 -0500

    Implement download feature

--- base/tests.py
+++ base/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from accounts import user
+from models import food, macro_day
+from django.test import Client
 
 # Create your tests here.
+class emptyUserTestCase(TestCase):
